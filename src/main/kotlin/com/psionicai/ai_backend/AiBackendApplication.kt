aace6c9ff5cc163c2795bb5c18bd3cf234a43312
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-09-05 15:32:15 +0900

    Initial commit

---
+++ src/main/kotlin/com/psionicai/ai_backend/AiBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.psionicai.ai_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/psionicai/ai_backend/AiBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.psionicai.ai_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
