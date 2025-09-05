71429b70fcfe4b76efaf3bebb24325ae0d24a534
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2025-09-05 16:45:03 +0900

    chore: 전역 CORS 설정 추가

---
+++ src/main/kotlin/com/psionicai/ai_backend/global/config/CorsConfig.kt
@@ -0,0 +1,23 @@
+package com.psionicai.ai_backend.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
+
+@Configuration
+class CorsConfig {
+    @Bean
+    fun corsFilter(): CorsFilter {
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOrigins = listOf("*")
+        config.allowedMethods = listOf("*")
+        config.allowedHeaders = listOf("*")
+
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        return CorsFilter(source)
+    }
+}
